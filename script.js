488f4d5e0d9a4fe31008dc236afa872911e6889a
arc4dex <pedroiago.bernardes@gmail.com>
2022-03-29 17:42:21 -0300

    Feito parcialmente o CSS e HTMLe e função de escutar as cores

---
+++ script.js
@@ -0,0 +1,11 @@
+const secaoJogo = document.getElementById('gameGenius')
+
+function randomizaColor(vermelho, verde, azul, amarelo){
+
+}
+
+secaoJogo.addEventListener('click', function(event){
+  const clicouCor = event.target.id
+  console.log(clicouCor)
+})
+
