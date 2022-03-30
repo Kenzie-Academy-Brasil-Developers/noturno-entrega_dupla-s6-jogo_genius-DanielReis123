7801b0d2a7c4787d29254904ddef9f8c726e1540
arc4dex <pedroiago.bernardes@gmail.com>
2022-03-30 18:39:59 -0300

    Adicionado função de sorteio de cores, click jogar, click nas cores, e função de comparação de arrays do usuario com o Genius

--- script.js
+++ script.js
@@ -1,11 +1,47 @@
-const secaoJogo = document.getElementById('gameGenius')
+const secaoJogo = document.getElementById('gameGenius');
+const botaoJogar = document.getElementById('btJogar');
+const nivel = document.getElementById('nivel');
+const corVerde = document.getElementById('1');
 
-function randomizaColor(vermelho, verde, azul, amarelo){
+nivel.innerText = 0
 
-}
+const arrResultRandomColor = []
+let coresClicadas = []
 
-secaoJogo.addEventListener('click', function(event){
-  const clicouCor = event.target.id
-  console.log(clicouCor)
+let max = 4 + parseInt(nivel.innerText)
+
+botaoJogar.addEventListener('click', function(randomColor){
+  for(let i = 0; i < max; i++){
+    arrResultRandomColor.push(Math.floor(Math.random() * 4 + 1))
+  }
+    coresClicadas = []
 })
 
+secaoJogo.addEventListener('click', function(event){
+  
+  const clicouCor = event.target.id
+  coresClicadas.push(parseInt(clicouCor))
+
+  if(comparacao()) {
+    console.log('passou')
+  } else {
+    alert("Tente novamente")
+  }
+})
+
+function comparacao(){
+  let verifica = true
+
+  for(let i = 0; i < coresClicadas.length; i++){
+    if(coresClicadas[i] !== arrResultRandomColor[i]){
+      verifica = false
+    }
+  }
+  return verifica
+}
+
+function incrementoNv(){
+}
+
+console.log(arrResultRandomColor)
+console.log(coresClicadas)
